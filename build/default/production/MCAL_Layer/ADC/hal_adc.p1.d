4c7b520bedf9087339c7b246594c47b46f0552a7
Kareem Taha <82422224+KareemTaha8178@users.noreply.github.com>
2022-10-10 18:48:38 +0200

    Add files via upload

---
+++ build/default/production/MCAL_Layer/ADC/hal_adc.p1.d
@@ -0,0 +1,8 @@
+build/default/production/MCAL_Layer/ADC/hal_adc.p1:  \
+MCAL_Layer/ADC/hal_adc.c  \
+MCAL_Layer/ADC/hal_adc.h  \
+MCAL_Layer/ADC/../std_types.h  \
+MCAL_Layer/ADC/../std_libraries.h  \
+MCAL_Layer/ADC/../compiler.h  \
+MCAL_Layer/ADC/../device_config.h  \
+MCAL_Layer/ADC/hal_adc_cfg.h 
---
+++ build/default/production/MCAL_Layer/Interrupt/hal_interrupt.p1.d
@@ -0,0 +1,8 @@
+build/default/production/MCAL_Layer/Interrupt/hal_interrupt.p1:  \
+MCAL_Layer/Interrupt/hal_interrupt.c  \
+MCAL_Layer/Interrupt/hal_interrupt.h  \
+MCAL_Layer/Interrupt/../std_types.h  \
+MCAL_Layer/Interrupt/../std_libraries.h  \
+MCAL_Layer/Interrupt/../compiler.h  \
+MCAL_Layer/Interrupt/../device_config.h  \
+MCAL_Layer/Interrupt/hal_interrupt_cfg.h 
